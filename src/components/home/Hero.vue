4cae6da6828a65d883b5a46b7b2b8da5ead13587
Simon Duchaine <simon.duchaine@hotmail.com>
2022-09-07 12:41:01 -0400

    hero accueil en 740px de haut

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -1,13 +1,16 @@
 <template>
   <section class="hero is-primary is-medium">
-    <g-image :src="$static.HeroImg.hero_image" height="740"/>
+    <g-image :src="$static.HeroImg.hero_image" class="hero-background" style="height:740px;"/>
+    <div class="hero-body">
+      <div class="container"></div>
+    </div>
   </section>
 </template>
 
 <static-query>
 query HeroImg {
   HeroImg: settings(path: "/data/site/") {
-    hero_image (height: 740, quality: 100)
+    hero_image (quality: 100)
   }
 }
 </static-query>
@@ -20,8 +23,29 @@ export default {
 
 <style scoped lang="scss">
 .hero {
+  height: 740px;
+  &.has-background {
+    position: relative;
+    overflow: hidden;
+  }
+
+  &-background {
+    position: absolute;
+    object-fit: cover;
+    object-position: top;
+    width: 100%;
+    height: 100vh;
+
+    &.is-transparent {
+      opacity: 0.7;
+    }
+  }
+
   &.is-medium {
-  
+    .hero-body {
+      padding-bottom: 8.5rem;
+      padding-top: 8.5rem;
+    }
   }
 }
 </style>
